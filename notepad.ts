07b59ca2811892e9061e761ea5e97e26f6853b95
JUMTBOX <yjey12@naver.com>
2023-05-18 13:16:56 +0900

    add abstract class

---
+++ notepad.ts
@@ -0,0 +1,25 @@
+enum User {
+  guest = 0,
+  member = 1,
+  admin = 2,
+}
+
+interface Info {
+  name: string;
+  id: string;
+  password: string;
+  age: number;
+}
+
+abstract class SmartContract {
+  constructor(
+    private contract: string,
+    private userInfo: Info,
+    public userNum: User
+  ) {}
+  getUSerInfo() {
+    return `${this.userInfo}`;
+  }
+}
+
+class NewContract extends SmartContract {}
