7f4ee328143690044e211836513933e8258b8b67
JUMTBOX <yjey12@naver.com>
2023-06-07 11:34:20 +0900

    ...

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+const normalRouter = require("./route");
+
+app.use("/home", normalRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is open at PORT${PORT}`);
